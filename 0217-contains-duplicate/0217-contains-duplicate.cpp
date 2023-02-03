b2b0add5e01b2e77af23c43762199ea564859b53
Walaa AbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-03 03:10:56 +0200

    Time: 211 ms (20.42%), Space: 70.6 MB (11.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+   bool containsDuplicate(vector<int>& nums) {
+    map<int, int>frq;
+    for (int i = 0; i < nums.size() ; i++) {
+        frq[nums[i]]++;
+        if (frq[nums[i]] > 1)return true;
+    }
+    return false;
+}
+};
