c749d783c31dcb62117c082b5ccabc681e993a4b
Walaa AbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-05 16:50:42 +0200

    Time: 13 ms (82.16%), Space: 8.5 MB (92.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-	vector<int> findAnagrams(string s, string p) {
-		int n = s.size(), m = p.size(), stIdx = 0;
-		vector<int>ans;//to store start idxs of all anagrams of string
-		if (n < m)return {};// if the sz of string s smaller than p ,then there are no  Anagrams in a String 
-		vector<int>frqS(26, 0), frqP(26, 0), clear(26, 0);
-		// store frq of chars in p to search if there is an anagram like it or not
-		for (int i = 0; i < m; i++)frqP[p[i] - 'a']++;
-		for (int i = 0; i <= n - m; i++) {
-			stIdx = i;// to store start idx if there is an anagram
-			//loop to count frqs of each substr in s which has sz equal to sz of p
-			for (int j = i; j < i + m; j++) {
-				frqS[s[j] - 'a']++;
-			}
-			// if this substr has same frq as p, then it is an anagram and then push first idx
-			if (frqS == frqP)ans.push_back(stIdx);
-			frqS = clear;// clear frqs2 to count frq of another substr (anagram)
-		}
-		return ans;
-	}
+    vector<int> findAnagrams(string& s, string& p) {
+        vector < int > freq_s(26), freq_p(26);
+        for(auto& c : p) freq_p[c - 'a']++;
+        vector < int > ans;
+        int l = 0, r = 0;
+        while(r < s.size()){
+            while(r < s.size() && r - l < p.size()) freq_s[s[r++] - 'a']++; 
+            if(freq_s == freq_p) ans.push_back(l);
+            freq_s[s[l++] - 'a']--;
+        }
+        return ans;
+    }
 };
