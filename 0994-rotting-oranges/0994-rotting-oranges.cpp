b917068861544070174e138940845656c4a42ec6
Walaa AbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-06 21:39:31 +0200

    Time: 7 ms (78.11%), Space: 13 MB (84.88%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,30 +1,41 @@
 class Solution {
+private:
+    queue<pair<int, int>> q;
+    int fresh, ans = -1;
+    int dx[4] = { 0,0,-1,1 };
+    int dy[4] = { -1,1,0,0 };
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int minutes = 2;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 2) {
-                    dfs(grid, i, j, minutes);
+                    q.push({ i,j });
+                }
+                if (grid[i][j] == 1)fresh++;
+            }
+        }
+       // cout<<fresh<<' '<<q.size();
+        while (!q.empty()){
+            int size = q.size();
+            while (size--) {
+                pair<int, int>parent = q.front();
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int x = parent.first + dx[i];
+                    int y = parent.second + dy[i];
+                    if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        fresh--;
+                        q.push({ x,y });
+                        //cout<<fresh<<' ';
+                    }
                 }
             }
+            ans++;
         }
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1)return -1;
-                else minutes =max(minutes, grid[i][j]);
-            }
-        }
-        return minutes - 2;
-    }
-private:
-    void dfs(vector<vector<int>>& grid, int i, int j,int minutes) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0)return;
-        if (grid[i][j] > 1 && grid[i][j] < minutes) return;
-        grid[i][j] = minutes;
-        dfs(grid, i - 1, j, minutes + 1);
-        dfs(grid, i + 1, j, minutes + 1);
-        dfs(grid, i, j - 1, minutes + 1);
-        dfs(grid, i, j + 1, minutes + 1);
+       // cout<<ans;
+        if (fresh > 0)return -1;
+        if (ans == -1)return 0;
+        return ans;
     }
 };
