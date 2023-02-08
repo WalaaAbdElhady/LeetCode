cfd8594e59c423827ae30ac74bf1ff12158b9596
Walaa AbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-08 17:52:12 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (17.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1)return 1;
-        int first=1,second=1,third=0;
-        for(int i=2;i<=n;i++){
-            third=first+second;
-            first=second;
-            second=third;
-        }
-        return third;
+      vector<int>dp(n+1,-1);
+      return solve(n,dp);
+    }
+    int solve(int n,vector<int>&dp){
+        if(n<=1)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
 };
