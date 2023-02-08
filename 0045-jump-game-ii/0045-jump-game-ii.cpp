cbcc8d2a2a9abccd51a572a1ee2b33026617b053
Walaa AbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-08 03:42:45 +0200

    Time: 15 ms (84.34%), Space: 16.5 MB (84.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,20 @@
+//Author: Walaa AbdElhady
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int l = 0, r = 0, ans = 0;
+        //two pointers to move
+        int l = 0, r = 0, numOfJump = 0;
         while (r < nums.size() - 1) {
-            int maxJump = 0;
+            int maxJump = 0;// to store max jump of ele
             for (int i = l; i <= r; i++) {
+                // Update the farthest reachable index of this jump
                 maxJump = max(maxJump, i + nums[i]);
             }
-            l = r + 1;
-            r = maxJump;
-            ans++;
+            l = r + 1;//store the start idx of the next jump
+            r = maxJump;//store  the farthest  idx of the next jump
+            numOfJump++;// increase numOfJump by one
         }
-        return ans;
+        //min numOfJump
+        return numOfJump;
     }
 };
