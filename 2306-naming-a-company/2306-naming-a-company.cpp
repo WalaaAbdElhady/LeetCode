e69a50c70ec913df1488500ef403948758a7e4a3
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-09 21:04:31 +0200

    Time: 2607 ms (5.69%), Space: 86.7 MB (21.95%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+	map<char, set<string>>words;
+	for (string& s : ideas) {
+		words[s[0]].insert(s.substr(1, s.size()));
+	}
+	long long numberOfValidNames = 0;
+	for (auto& c1 : words) {
+		for (auto& c2 : words) {
+			int intersection = 0;
+			if (c1 != c2) {
+				for (auto& word : c1.second) {
+					if (c2.second.find(word) != c2.second.end()) {
+						intersection++;
+					}
+				}
+				int validInC1 = c1.second.size() - intersection;
+				int validInC2 = c2.second.size() - intersection;
+				numberOfValidNames +=validInC1 * validInC2;
+			}
+		}
+	}
+	return numberOfValidNames;
+}
+};
