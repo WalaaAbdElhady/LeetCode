8a5b8cf406e46e774a49cab4bc9574a892034439
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-10 22:25:48 +0200

    Time: 8 ms (66.80%), Space: 6.4 MB (98.37%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+    string ans(num1.size() + num2.size(), '0');// maxSize
+    for (int i = num1.size() - 1; i >= 0; i--) {
+        for (int j = num2.size() - 1; j >= 0; j--) {
+            int tmp = (num1[i] - '0') * (num2[j] - '0') + ans[i + j + 1] - '0';
+            ans[i + j + 1] = tmp % 10 + '0';
+            ans[i + j] += tmp / 10;// carry
+        }
+    }
+    for (int i = 0;i<ans.size();i++) {
+        if (ans[i] != '0')return ans.substr(i);
+    }
+    return "0";
+}
+};
