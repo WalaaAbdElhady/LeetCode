aa07ffee5026aa679df6db0f9fb77b4b194bc3ab
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-12 00:31:57 +0200

    Time: 258 ms (5.12%), Space: 52.5 MB (5.12%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -4,29 +4,34 @@ private:
 public:
 	vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
 		vector<int>ans(n,-1);
+        ans[0] = 0;
 		vector<vector<pair<int, int>>>adj(n, vector<pair<int, int>>(n));
 
 		for (auto& node : redEdges)adj[node[0]].push_back({ node[1], 1 });
 		for (auto& node : blueEdges)adj[node[0]].push_back({ node[1], 0 });
 		//src = 0
 		queue<vector<int>>q;
-		q.push({ 0,0,1 });
-		q.push({ 0,0,0 });
+		q.push({ 0,1 });
+		q.push({ 0,0 });
+        int dep = -1;
 		while (!q.empty()) {
-			auto p = q.front();
-			q.pop();
-			int from = p[0], to = p[1], color = p[2];
-			vis[from][color] = true;
-			for (auto& ch : adj[from]) {
-				if (!vis[ch.first][ch.second] && ch.second != color) {
-					if (ans[ch.first] == -1) {
-						ans[ch.first] = to + 1;
-					}
-					q.push({ ch.first,to + 1,ch.second });
-				}
-			}
+            int sz=q.size();
+            dep++;
+            while(sz--){
+                auto p = q.front();
+                q.pop();
+                int node = p[0], color = p[1];
+                vis[node][color] = true;
+                for (auto& ch : adj[node]) {
+                    if (!vis[ch.first][ch.second] && ch.second != color) {
+                        if (ans[ch.first] == -1) {
+                            ans[ch.first] = dep + 1;
+                        }
+                       q.push({ ch.first,ch.second});
+                    }
+                }
+            }
 		}
-        ans[0]=0;
 		return ans;
 	}
 
