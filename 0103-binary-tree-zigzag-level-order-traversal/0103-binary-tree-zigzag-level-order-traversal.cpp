26fc7ac6471c0ec9e45bbfc87bb15d36aff1e148
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-02-19 19:49:16 +0200

    Time: 0 ms (100.00%), Space: 12.1 MB (48.40%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,33 @@
+// for this problem we need to traverse tree from left to right and from right to left so =>
+// we need to traverse it level by level (bfs)
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        // base case => when the tree is empty
+        if(!root)return ans;
+        int level = 0;
+        queue<TreeNode*>q;
+        // add the root to the queue
+        q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int>tmp;
+            while (sz--) {
+              // get the current node and remove it from the queue
+                TreeNode* parent = q.front();
+                q.pop();
+                tmp.push_back(parent->val);
+                if (parent->left)q.push(parent->left);
+                if (parent->right)q.push(parent->right);
+            }
+            // when the level is odd we need to reverse nodes
+            if (level & 1)reverse(tmp.begin(), tmp.end());
+            ans.push_back(tmp);
+            level ++;
+        }
+        // root after zigzag level order traversal
+        return ans;
+    }
+
+};
