eaebbd94218dec61090ffe82b50eb6ba50c08d72
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-04-06 23:50:57 +0200

    Time: 3 ms (70.99%), Space: 6 MB (21.10%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
+    int dp[31];
 public:
     int fib(int n) {
-        if(n<=1)return n;
-        return fib(n-1)+fib(n-2);
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+    int solve(int n){
+      if(n<=1)return n;
+      if(dp[n]!=-1)return dp[n];
+      return dp[n]=solve(n-1)+solve(n-2);
     }
 };
