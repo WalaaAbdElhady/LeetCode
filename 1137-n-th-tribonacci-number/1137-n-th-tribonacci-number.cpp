53272e9ae12fb0ad980dad6ae614b7f715ce1209
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-04-06 23:54:13 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (77.05%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
+    int dp[40];
 public:
     int tribonacci(int n) {
-        vector<long long>v(38);
-        v[0]=0;
-        v[1]=v[2]=1;
-        for(int i=3;i<38;i++)v[i]=v[i-1]+v[i-2]+v[i-3];
-        return v[n];
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+     int solve(int n){
+      if(n==0)return 0;
+      if(n==1 || n==2)return 1;
+      if(dp[n]!=-1)return dp[n];
+      return dp[n]=solve(n-1)+solve(n-2)+solve(n-3);
     }
 };
