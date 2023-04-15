7a1ea57e07fdd7d0037b54e0a30c52021c397185
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-04-15 14:02:40 +0200

    Time: 49 ms (98.66%), Space: 10.9 MB (71.27%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         memset(dp,-1,sizeof(dp));
         return solve(0,s.size()-1,s);
     }
-    int solve(int i,int j,string& s){
+    int solve(int i,int j,string &s){
         if(i>j)return 0;
         if(i==j)return 1;
         int &ret=dp[i][j];
