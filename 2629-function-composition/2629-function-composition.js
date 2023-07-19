19592da1475d4c1b4da7aa707c4b2e46dca5c381
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-07-19 12:39:31 +0300

    Time: 70 ms (70.13%), Space: 43.2 MB (64.23%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,12 +3,18 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	return function(x) {
-        for(const fn of functions.reverse()){
-            x = fn(x);
-        }
-        return x;
+   // const fn=(acc,f)=>f(acc);
+    return function(x){
+        return functions.reduceRight(function(acc,f){
+            return f(acc);
+        },x);
     }
+	// return function(x) {
+	// for(const fn of functions.reverse()){
+	// x = fn(x);
+	// }
+	// return x;
+	// }
 };
 
 /**
