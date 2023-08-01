f24eeaa39ef6595d81b9fc9e7b529ea85f57c71d
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-08-01 15:26:08 +0300

    Time: 19 ms (87.17%), Space: 9.9 MB (49.76%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,21 +1,20 @@
 class Solution {
-private:
     vector<vector<int>>ans;
-    vector<int>v;
+    vector<int>tmp;
 public:
-    void solve(int i, int n, int k) {
-        if (v.size() == k) {
-            ans.push_back(v);
-            return;
-        }
-        for (int j = i; j <= n; j++) {
-            v.push_back(j);
-            solve(j + 1, n, k);
-            v.pop_back();
-        }
-    }
     vector<vector<int>> combine(int n, int k) {
-        solve(1, n, k);
+        solve(1,n,k);
         return ans;
+    }
+    void solve(int i,int n,int k){
+        if(tmp.size()==k){
+         ans.push_back(tmp);
+         return;
+        }
+        for(int j=i;j<=n;j++){
+            tmp.push_back(j);
+            solve(j+1,n,k);
+            tmp.pop_back();
+        }
     }
 };
