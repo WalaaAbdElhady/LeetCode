16316cbba6fed98f7be10c5748a2a0aee46c27dc
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2023-08-02 18:06:05 +0300

    Time: 0 ms (100.00%), Space: 7.9 MB (39.26%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,25 @@
 class Solution {
-private:
-vector<vector<int>>ans;
-vector<int>v;
+    vector<int>tmp;
+    vector<vector<int>>ans;
 public:
-    void solve(vector<int>& nums,vector<bool>& vis){
-        if(v.size()==nums.size()){
-            ans.push_back(v);
-            return;
-        }
-        for(int j=0;j<nums.size();j++){
-          if(!vis[j]){
-             vis[j]=true;
-             v.push_back(nums[j]);
-             solve(nums,vis);
-             v.pop_back();
-             vis[j]=false;
-          }
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<bool>vis(nums.size());
         solve(nums,vis);
         return ans;
+    }
+    void solve(vector<int>& nums,vector<bool>& vis){
+        if(tmp.size()==nums.size()){
+            ans.push_back(tmp);
+            return;
+        }
+        for(int j=0;j<nums.size();j++){
+            if(!vis[j]){
+                vis[j]=true;
+                tmp.push_back(nums[j]);
+                solve(nums,vis);
+                vis[j]=false;
+                tmp.pop_back();
+            }
+        }
     }
 };
