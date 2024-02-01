65bad368b22e432daf06ffa9b8816250199e13e6
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2024-02-01 20:42:25 +0200

    Time: 158 ms (64.73%), Space: 119.7 MB (38.96%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -5,22 +5,12 @@ public:
         int n=nums.size();
         if(n==1)return ans;
         sort(nums.begin(),nums.end());
-        vector<int>tmp,cur;
-        int l=nums[0],r=nums[1];
-        for(int i=0;i<n;i++){
-            if(r-l<=k && (r-nums[i])<=k && tmp.size()<3){
-              r=nums[min(i+1,n-1)];
-              tmp.push_back(nums[i]);
-           }else {
-                l=nums[i];
-                if(tmp.size()<3)return {};
-                ans.push_back(tmp);
-                tmp=cur;
-                tmp.push_back(nums[i]);
-           }
-           // cout<<l<<' '<<r<<' '<<nums[i]<<"\n";
+        //Divide the array into one or more arrays of size 3
+        for(int i=0;i<n;i+=3){
+           if(nums[i+2]-nums[i]>k)return {};
+            ans.push_back({nums[i],nums[i+1],nums[i+2]});
         }
-        if(tmp.size())ans.push_back(tmp);
+        
         return ans;
 }
 };
