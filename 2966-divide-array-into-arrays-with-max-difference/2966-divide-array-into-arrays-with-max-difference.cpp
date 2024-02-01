4f2c25a5ca4bb9fe4436f88baf60e9fdcda94b7c
WalaaAbdElhady <120207533+WalaaAbdElhady@users.noreply.github.com>
2024-02-01 20:40:35 +0200

    Time: 151 ms (79.24%), Space: 119.7 MB (38.96%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>>ans,ans2;
+        vector<vector<int>>ans;
         int n=nums.size();
         if(n==1)return ans;
         sort(nums.begin(),nums.end());
         vector<int>tmp,cur;
-        int l=nums[0],r=nums[1],ok=0;
+        int l=nums[0],r=nums[1];
         for(int i=0;i<n;i++){
             if(r-l<=k && (r-nums[i])<=k && tmp.size()<3){
               r=nums[min(i+1,n-1)];
               tmp.push_back(nums[i]);
            }else {
                 l=nums[i];
-                if(tmp.size()<3){
-                    ok=1;
-                    break;
-                }
+                if(tmp.size()<3)return {};
                 ans.push_back(tmp);
                 tmp=cur;
                 tmp.push_back(nums[i]);
@@ -24,7 +21,6 @@ public:
            // cout<<l<<' '<<r<<' '<<nums[i]<<"\n";
         }
         if(tmp.size())ans.push_back(tmp);
-        if(ok)return ans2;
         return ans;
 }
 };
